c283a668a1871a0ad1f4d3d3faade3e4a670a637
Chintan_06 <159480965+Chintansgowda@users.noreply.github.com>
2025-11-13 20:06:11 +0530

    Remove unused security group and subnet ID
    Remove unused VPC security group and subnet ID.

--- 06_COUNT_META_ARGUMENT/main.tf
+++ 06_COUNT_META_ARGUMENT/main.tf
@@ -8,8 +8,6 @@ resource "aws_instance" "my-instance" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    vpc_security_group_ids = [var.security_group_id]
-    subnet_id = var.subnet_id
     
     root_block_device {
         volume_size = var.env == "prod" ? 20 : var.volume_size # conditinal-expression
